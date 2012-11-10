6715f0cfce8ec17102182affb58476e2ce4f7c7c
mafintosh <mathiasbuus@gmail.com>
2012-11-11 00:01:53 +0100

    more cmds

--- public/browser.js
+++ public/browser.js
@@ -5,18 +5,34 @@ window.onload = function() {
 	var stream = shoe('http://'+(location.hostname || 'localhost')+'/spy');
 	var spy = spies();
 
-	spy.on('where', function() {
-		spy.log('i am the browser!');
+	spy.on('whoami', function() {
+		spy.log('i am the browser');
 	});
-	spy.on('color', function(color) {
+	spy.on('full', function(color) {
 		document.body.style.backgroundColor = color;
 		spy.log('i am now '+color);
 	});
-	spy.on('print', function() {
+	spy.on('text', function() {
 		var msg = Array.prototype.slice.call(arguments).join(' ');
-		document.body.innerHTML += msg+'<br>';
+		document.getElementById('text').innerHTML = msg;
 		spy.log('i said '+msg);
 	});
+	spy.on('barrel-roll', function() {
+		document.getElementById('placeholder').className = 'barrel-roll';
+		setTimeout(function() {
+			document.getElementById('placeholder').className = '';
+		}, 1500);
+		spy.log('do a barrel roll');
+	});
+	spy.on('window', function() {
+		spy.log({
+			x: window.screenX,
+			y: window.screenY,
+			width: window.outerWidth,
+			height: window.outerHeight
+		});
+	});
+
 
 	stream.pipe(spy).pipe(stream);
 };
